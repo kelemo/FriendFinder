b767ff2a16b38f58c629266076c05d6206955baa
Kadiro Elemo <you@example.com>
2018-03-03 10:07:00 -0600

    FriendFinder

--- server.js
+++ server.js
@@ -1,26 +1,39 @@
-// Pull in required dependencies
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
-// Configure the Express application
-var app = express();
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
 var PORT = process.env.PORT || 8080;
 
-// Expose the public directory to access CSS files
+// Sets up the Express app to handle data parsing
 app.use(express.static(path.join(__dirname, './app/public')));
 
-// Add middleware for parsing incoming request bodies
+// This parses incoming request bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
-// Add the application routes
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 require(path.join(__dirname, './app/routing/apiRoutes'))(app);
 require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
-// Start listening on PORT
-app.listen(PORT, function() {
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+app.listen(PORT, function() {app.listen(PORT, function() {
   console.log('Friend Finder app is listening on PORT: ' + PORT);
 });
 
